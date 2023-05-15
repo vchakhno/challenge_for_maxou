6bb9610c9b9f2c06dffc63efd870b2871fbc3fc1
Velimir Chakhnovski <velimir2707@gmail.com>
2023-05-15 05:23:59 +0200

    Added uninitialized

---
+++ solutions/uninitialized.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   uninitialized.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 03:03:13 by vchakhno          #+#    #+#             */
+/*   Updated: 2023/05/15 05:22:03 by vchakhno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+// Consigne:
+// Tu dois faire en sorte que le programme print "[5, 6, 7]" en écrivant
+// seulement du code entre les accolades d'init_values.
+
+int	init_values(void)
+{
+	int	values[3] = {5, 6, 7};
+}
+
+int	print_values(void)
+{
+	int	values[3];
+
+	printf("[%d, %d, %d]\n", values[0], values[1], values[2]);
+}
+
+int	main(void)
+{
+	init_values();
+	print_values();
+}
